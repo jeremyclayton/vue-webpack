0637e9be25730024d8208d34fd7396bb3ed58511
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-05-02 12:51:43 -0600

    initial commit

---
+++ src/components/AppNav.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="app">
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <h3 class="text-center">Header</h3>
+    </div>
+  </nav>
+</div>
+</template>
+
+<script>
+// import { isLoggedIn, login, logout } from '../../utils/auth';
+
+export default {
+  name: 'app-nav',
+};
+</script>
+
+<style scoped>
+/*.navbar-right {
+  margin-right: 0px !important
+}
+
+.log {
+  margin: 5px 10px 0 0;
+}*/
+</style>
---
+++ src/components/Book.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <app-nav></app-nav>
+  <h1 style="padding-top: 10em;">Hello From Book Component</h1>
+</div>
+</template>
+
+<script>
+import AppNav from './AppNav';
+export default {
+  name: 'book',
+  components: {
+    AppNav,
+  },
+};
+</script>
+
+<style lang="css">
+</style>
